efaa45e0ca9fb615596cda12a64898b934969119
Omri Levy <omri2861@gmail.com>
2020-11-15 18:03:31 +0200

    Working on tokenizing expressions

--- index.js
+++ index.js
@@ -18,12 +18,43 @@ function calculatorPress(button) {
       currentNumber += command;
     }
   } else if (OPERATORS.includes(command)) {
-    expression += currentNumber + command;
+    expression += currentNumber + " " + command + " ";
     currentNumber = "0";
     $("#expression").text(expression);
   }
+  else if (command === "=") {
+      expression += currentNumber;
+      $("#expression").text(expression);
+      console.log(evaluateExpression(expression));
+  }
 
   $("#calc-screen").text(currentNumber);
+}
+
+class BinaryOperator {
+  constructor(operator, level, evaluate) {
+    this.operator = operator;
+    this.level = level;
+    this.evaluate = evaluate;
+  }
+}
+
+var operators = [
+  new BinaryOperator("+", 1, function (a, b) {
+    return a + b;
+  }),
+  new BinaryOperator("-", 1, function (a, b) {
+    return a - b;
+  }),
+];
+
+function evaluateExpression(expression) { 
+    elements = expression.split(" ");
+    evaluated = [];
+    for (i = 1; i < elements.length; i++) {
+        console.log(i);
+    }
+    return elements.length;
 }
 
 $(".calc-button").on("click", calculatorPress);
